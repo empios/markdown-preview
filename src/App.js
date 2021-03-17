d02f22496289b4860cc201bfdd75ad9f61af4feb
empios <pawelwlodarczyk97@yahoo.com>
2021-03-17 23:31:29 +0100

    Add readme and gif

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Grid, Typography, Container} from "@material-ui/core";
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+let marked = require("marked");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            markdown: "",
+        };
+    }
+    updateMarkdown(markdown) {
+        this.setState({ markdown });
+    }
+
+  render(){
+    return (
+        <Container>
+            <Grid container justify={"center"} style={{marginTop: "2em"}}>
+                <Grid item >
+                    <Typography variant={"h2"}>MarkDown Previewer!</Typography>
+                </Grid>
+            </Grid>
+            <Grid container justify={"center"} style={{marginTop: "3em", textAlign: "center"}}>
+                <Grid item xs={6}>
+                    <Typography variant={"h5"}>Markdown input</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant={"h5"}>HTML output</Typography>
+                </Grid>
+            </Grid>
+            <Grid container style={{marginTop: "2em", textAlign: "center"}}>
+                <Grid item xs={6}>
+                    <TextareaAutosize value={this.state.markdown} onChange={(e) => {this.updateMarkdown(e.target.value);}} aria-label="empty textarea" placeholder="Input" rows={15} />
+                </Grid>
+                <Grid item xs={6} style={{backgroundColor: "#394F8A", color: "white"}}>
+                    <div dangerouslySetInnerHTML={{ __html: marked(this.state.markdown) }} style={{ height: "100%", width: "60%", margin: "auto"}} />
+                </Grid>
+            </Grid>
+        </Container>
+    );}
 }
-
-export default App;
